f550f0b679693d6fe74e9266112886862e9c165f
Nathan N <Nitharios@users.noreply.github.com>
2017-08-24 18:35:35 -1000

    Created calculatorModule and added basic calculator functions and load method

--- calculator.js
+++ calculator.js
@@ -5,6 +5,13 @@
  * @variable PRIVATE { Number } `total`
  * @return {object} `calculator` object that can be used
  */
+ var calculatorModule = (function() {
+
+    var memory, total;
+
+    return calculator = {
+      load: setTotal
+    }
 
 
   /**
@@ -12,54 +19,76 @@
    * @param  { Number } x
    * @return { Number }    current total
    */
-
+    function setTotal(x) {
+      total = x
+      return total
+   }
 
   /**
    * Return the value of `total`
    * @return { Number }
    */
+    function returnTotal() {
+      return total
+   }
 
 
   /**
    * Sums the value passed in with `total`
    * @param { Number } x
    */
-
+    function add(x) {
+      total += x
+    }
 
   /**
    * Subtracts the value passed in from `total`
    * @param  { Number } x
    */
-
+    function subtract(x) {
+      total -= x
+    }
 
   /**
    * Multiplies the value by `total`
    * @param  { Number } x
    */
-
+    function multiply(x) {
+      total *= x
+    }
 
   /**
    * Divides the value passing in by `total`
    * @param  { Number } x
    */
-
+    function divide(x) {
+      total /= x
+    }
 
   /**
    * Return the value stored at `memory`
    * @return { Number }
    */
-
+    function returnMemory() {
+      return memory
+    }
 
   /**
    * Stores the value of `total` to `memory`
    */
-
+    function toMemory() {
+      memory = total
+    }
 
   /**
    * Clear the value stored at `memory`
    */
+    function clearMemory() {
+      var memory
+    }
 
   /**
    * Validation
    */
 
+})
